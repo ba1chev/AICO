fe174253c0dc4a51d3d63fa0bbc39a9c580cd1ef
ba1chev <baychevyoan@gmail.com>
2026-05-29 23:07:59 +0300

    Exposed charts module via barrel index

---
+++ src/core/charts/index.ts
@@ -0,0 +1,8 @@
+export { ChartComponent } from './ChartComponent';
+export type { ChartAccessibility } from './ChartComponent';
+export { BarChart } from './BarChart';
+export type { BarDatum, BarChartOptions } from './BarChart';
+export { PieChart } from './PieChart';
+export type { PieDatum, PieChartOptions } from './PieChart';
+export { LineChart } from './LineChart';
+export type { LinePoint, LineChartOptions } from './LineChart';
